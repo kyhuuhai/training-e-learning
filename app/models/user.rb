9f780c85c9dde5e6e7f9a3d6a9a3ccf6d1d66312
Chibigo <vietle1728@gmail.com>
2022-04-08 16:24:23 +0700

    create signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
     has_many :user_courses
     has_many :user_lessons
     has_many :user_words
     has_many :practices
+
+    validates :name,length: {maximum:50},presence:true
+    validates :email,length: {maximum:100}, presence: true, format: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+    uniqueness: {case_sensitive:false}
 end
