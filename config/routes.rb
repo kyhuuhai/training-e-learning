5000e21c41b6c854d57903567e257bd42c699e6d
TruongNQM <truong29102000@gmail.com>
2022-04-15 09:51:23 +0700

    Login admin to Page Admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get'login'=>'sessions#new'
   post'login'=>'sessions#create'
   delete'logout'=>'sessions#destroy'
+  get 'layoutadmin' => 'admin#index'
   resources :users
 end
